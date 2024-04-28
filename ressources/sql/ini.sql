e41ad029cbfed01a2160ed55774712322fd4ad8b
Ari33260 <baptiste.bordenave@etu.univ-st-etienne.fr>
2024-04-28 15:03:23 +0200

    Correction gitignore dosser ressources

---
+++ ressources/sql/ini.sql
@@ -0,0 +1,12 @@
+-- Création d'une table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+-- Insertion de données dans la table
+INSERT INTO users (username, email) VALUES
+('user1', 'user1@example.com'),
+('user2', 'user2@example.com'),
+('user3', 'user3@example.com');
